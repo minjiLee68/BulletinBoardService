a34f29a51d86c61bf976f7efd5ee00cba1e75d8b
minjiLee68 <dlalswl2598@gmail.com>
2022-03-05 22:14:07 +0900

    Initial Commit

---
+++ CalorieControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalorieControl
+//
+//  Created by 이민지 on 2022/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
